d7ee621416010aa662b6a8be7514967a48bfafaa
JCJ <jesse.jackson.c@gmail.com>
2020-08-23 22:33:55 -0500

    Added functionality to search button to store value in a variable. Also added functionality that allows user to hit 'enter' to submit a value

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
 
+    // Saving value from input
+    $("#inputBtn").on("click", function() {
+        var inputVal = $("#inputCity").val();
+        console.log(inputVal);
+
+        $("#inputCity").val("");
+    })
+
+    $("#inputCity").on("keypress", function (e) {
+        if (e.which == 13) {
+            var inputVal = $("#inputCity").val();
+            console.log(inputVal);
     
-}
+            $("#inputCity").val("");
+        }
+    })
+
+
+
+
+
+
+
+})
+
